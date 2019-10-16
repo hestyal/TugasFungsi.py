464e8f3cb81a5fd1f4c4c93db2043535a7d6435b
hestyal <49118867+hestyal@users.noreply.github.com>
2019-10-16 20:07:17 +0800

    2.py
    menyelesaikan soal tantangan

---
+++ 2.py
@@ -0,0 +1,15 @@
+import tugas 
+
+def main():
+	p = 3
+	v = 5
+
+	mb = tugas.massabenda(p, v)
+
+	print ("mencari massa benda")
+	print ("massa jenis\t: ", p)
+	print ("volume\t: ", v)
+	print ("massa benda\t= ", mb)
+
+if __name__ == "__main__":
+	main()
